e7c7f213549d11ee016508f30760a6a843e89c57
Viacheslav_Poliakov <polyakov_slava@ukr.net>
2023-10-06 13:41:54 +0300

    Initial commit

---
+++ src/main/java/com/epam/jgmp/customstarter/CustomStarterApplication.java
@@ -0,0 +1,13 @@
+package com.epam.jgmp.customstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomStarterApplication.class, args);
+    }
+
+}
